38c53009a58a4b3f920aa05d53e5a2678619bcab
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-25 19:57:40 +0530

    25Aug Commit2

--- client/src/Components/FeedDetails.js
+++ client/src/Components/FeedDetails.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {Banner2} from "../asset";
+import {BannerImage} from "../asset";
 import {useParams} from "react-router-dom";
 import {fetchFeedsDetails} from "../sanity";
+import {FaHeart} from "react-icons/fa";
 
 const FeedDetails = ()=>{
 
@@ -16,13 +17,13 @@ const FeedDetails = ()=>{
     
     return(
         <div className="w-screen h-auto flex flex-col items-center justify-center relative">
-            <div className="w-screen py-3 flex items-center justify-center relative ">
-                <img src={Banner2} alt="" className="object-cover" />
+            <div className="w-screen  flex items-center justify-center relative ">
+                <img src={BannerImage} alt="" className="object-cover" />
                 <div className="inset-0 bg-overlay-4"></div>
             </div>
 
-            <div className="w-full grid grid-cols-1 lg:grid-cols-2 px-8 lg:px-10 xl:px-32 gap-4">
-                <div className="flex flex-col items-start justify-start gap-4">
+            <div className="w-full grid grid-cols-1 lg:grid-cols-2 px-8 lg:px-10 xl:px-32 py-3">
+                <div className="flex flex-col items-start justify-start">
                     <div className="flex h-auto lg:h-500 xl:h-[800] items-center justify-center flex-col overflow-hidden rounded-md shadow-md">
                         {feed?.mainImage &&(
                             <img src={feed.mainImage.asset.url} alt="" className="w-full h-full object-cover object-center"/>
@@ -33,7 +34,29 @@ const FeedDetails = ()=>{
                         )}
                     </div>
                 </div>
-                <div>2</div>
+                <div className="flex items-center flex-col justify-start w-full gap-6">
+
+                    <div className="flex items-center justify-center gap-3">
+                        <img src={feed?.users?.photoURL} alt="" className="w-12 h-12 rounded-full object-cover shadow-md"/>
+                        <p className="text-lg text-primary font-semibold">{feed?.users?.displayName}</p>
+                    </div>
+
+                    <div className="flex items-center justify-center gap-3">
+                        <div className="flex items-center justify-center gap-2 px-2 py-1 rounded-md border border-red-200">
+                            <FaHeart className="text-red-500 text-lg"/>
+
+                            {feed?.collections?.length>0?(
+                                <p className="text-base text-primary font-semibold">
+                                    {feed?.collections?.length}
+                                </p>
+                            ):(
+                                <p className="text-base text-primary font-semibold">0</p>
+                            )}
+
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
